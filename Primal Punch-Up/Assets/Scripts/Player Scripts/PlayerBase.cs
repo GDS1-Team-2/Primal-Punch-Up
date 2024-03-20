a70b582426c860c5d22fec815bf417612a300e01
keegenwall-is <keegen.wallis@gmail.com>
2024-03-21 10:28:00 +1100

    Added more lizard animations

--- Primal Punch-Up/Assets/Scripts/Player Scripts/PlayerBase.cs
+++ Primal Punch-Up/Assets/Scripts/Player Scripts/PlayerBase.cs
@@ -2,32 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class PlayerBase : MonoBehaviour
 {
 
     public Animator anim;
     public Rigidbody rbody;
 
     private Vector3 moveDirection;
+    private Vector3 lastMoveDirection;
     public float speed = 10.0f;
-    public float rotateSpeed;
+    public float rotateSpeed = 720.0f;
+
+    public bool canMove;
+
+    public float lizardSpeed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         rbody = GetComponent<Rigidbody>();
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         ChangeDirection();
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            StartCoroutine(PlayerBasicAttack());
+        }
     }
 
     void FixedUpdate()
     {
-        Move();
+        if (canMove)
+        {
+            Move();
+        }
     }
 
     void ChangeDirection()
@@ -43,16 +57,29 @@ public class PlayerMove : MonoBehaviour
     {
         rbody.velocity = new Vector3(moveDirection.x * speed, 0.0f, moveDirection.z * speed);
 
-        Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
-
-        transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotateSpeed * Time.deltaTime);
+        if(moveDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotateSpeed * Time.deltaTime);
+        }
 
         if (rbody.velocity.z != 0 || rbody.velocity.x != 0)
         {
             anim.Play("LizardRun");
+            lastMoveDirection = moveDirection;
         } else
         {
             anim.Play("LizardIdle");
         }
+    }
+
+    IEnumerator PlayerBasicAttack()
+    {
+        canMove = false;
+        speed = 0;
+        anim.Play("LizardAttack1");
+        yield return new WaitForSeconds(anim.GetCurrentAnimatorStateInfo(0).length/2);
+        canMove = true;
+        speed = lizardSpeed;
     }
 }
