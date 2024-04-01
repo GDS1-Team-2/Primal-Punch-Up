0f3eaf04d400a238b7b8705c90443a2e65c4a047
DieFlugel233 <934615178@qq.com>
2024-04-01 21:29:55 +1100

    selection and camera

--- Primal Punch-Up/Assets/Script/CharacterSelectionManager.cs
+++ Primal Punch-Up/Assets/Script/CharacterSelectionManager.cs
@@ -3,26 +3,46 @@ using UnityEngine.SceneManagement;
 
 public class CharacterSelectionManager : MonoBehaviour
 {
-    public GameObject[] characterPrefabs;
-    private int selectedCharacterIndex = 0; 
-    public string playerPrefsKey = "SelectedCharacterIndexPlayer1";
+    public GameObject[] character1Prefabs;
+    private int selectedCharacter1Index = 0;
+    public string playerPrefsKey1 = "SelectedCharacterIndexPlayer1";
 
-    public void NextCharacter()
+    public GameObject[] character2Prefabs;
+    private int selectedCharacter2Index = 0;
+    public string playerPrefsKey2 = "SelectedCharacterIndexPlayer2";
+
+    public void NextCharacter1()
     {
-        selectedCharacterIndex++;
-        if (selectedCharacterIndex >= characterPrefabs.Length) selectedCharacterIndex = 0;
- 
+        selectedCharacter1Index++;
+        if (selectedCharacter1Index >= character1Prefabs.Length) selectedCharacter1Index = 0;
     }
 
-    public void PreviousCharacter()
+    public void PreviousCharacter1()
     {
-        selectedCharacterIndex--;
-        if (selectedCharacterIndex < 0) selectedCharacterIndex = characterPrefabs.Length - 1;
+        selectedCharacter1Index--;
+        if (selectedCharacter1Index < 0) selectedCharacter1Index = character1Prefabs.Length - 1;
+    }
+
+    public void NextCharacter2()
+    {
+        selectedCharacter2Index++;
+        if (selectedCharacter2Index >= character2Prefabs.Length) selectedCharacter2Index = 0;
+    }
+
+    public void PreviousCharacter2()
+    {
+        selectedCharacter2Index--;
+        if (selectedCharacter2Index < 0) selectedCharacter2Index = character2Prefabs.Length - 1;
     }
 
     public void StartGame()
     {
-        PlayerPrefs.SetInt(playerPrefsKey, selectedCharacterIndex);
+        PlayerPrefs.SetInt(playerPrefsKey1, selectedCharacter1Index);
+        PlayerPrefs.SetInt(playerPrefsKey2, selectedCharacter2Index);
+        PlayerPrefs.Save();
         SceneManager.LoadScene("PvPScene");
     }
+
 }
+
+
--- Primal Punch-Up/Assets/Script/PvPCharacterSpawner.cs
+++ Primal Punch-Up/Assets/Script/PvPCharacterSpawner.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class PvPCharacterSpawner : MonoBehaviour
 {
     public GameObject[] characterPrefabs;
-    public Transform spawnPointPlayer1; 
-    public Transform spawnPointPlayer2; 
+    public Transform spawnPointPlayer1;
+    public Transform spawnPointPlayer2;
 
     void Start()
     {
         int player1CharacterIndex = PlayerPrefs.GetInt("SelectedCharacterIndexPlayer1", 0);
-        int player2CharacterIndex = PlayerPrefs.GetInt("SelectedCharacterIndexPlayer2", 0); 
+        int player2CharacterIndex = PlayerPrefs.GetInt("SelectedCharacterIndexPlayer2", 0);
 
         Instantiate(characterPrefabs[player1CharacterIndex], spawnPointPlayer1.position, Quaternion.identity);
         Instantiate(characterPrefabs[player2CharacterIndex], spawnPointPlayer2.position, Quaternion.identity);
     }
 }
+
 
