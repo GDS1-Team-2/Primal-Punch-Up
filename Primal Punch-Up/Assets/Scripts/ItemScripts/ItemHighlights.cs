c1dc029755687672c75a8fd6fd7c04ed379defe4
speedyseal69 <111723174+speedyseal69@users.noreply.github.com>
2024-04-21 21:58:16 +1000

    item highlights and bg music

---
+++ Primal Punch-Up/Assets/Scripts/ItemScripts/ItemHighlights.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHighlights : MonoBehaviour
+{
+    public GameObject lightingPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newPrefab = Instantiate(lightingPrefab, this.transform.position, this.transform.rotation);
+        newPrefab.transform.parent = this.transform;
+        newPrefab.transform.localScale = new Vector3(3, 3, 3);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
