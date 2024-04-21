7f5afe536fdff5698c4d00850a11ee0386b8a753
keegenwall-is <keegen.wallis@gmail.com>
2024-04-21 12:07:01 +1000

    Lizard ability damage added

--- Primal Punch-Up/Assets/Scripts/Player Scripts/LizardUniqueAbility.cs
+++ Primal Punch-Up/Assets/Scripts/Player Scripts/LizardUniqueAbility.cs
@@ -8,17 +8,20 @@ public class LizardUniqueAbility : MonoBehaviour
     private PlayerBase baseScript;
     public SkinnedMeshRenderer smr;
     public ParticleSystem smokePrefab;
+    public SphereCollider smokeCollider;
 
     private bool abilityCD = false;
     private float cdTimer = 0.0f;
     public float cdLength = 5.0f;
     Vector3 smokeSpawnLoc = new Vector3(0, 2, 0);
+    public float invisTime = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         baseScript = GetComponent<PlayerBase>();
         smr = GetComponentInChildren<SkinnedMeshRenderer>();
+        smokeCollider = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
@@ -64,10 +67,30 @@ public class LizardUniqueAbility : MonoBehaviour
     {
         smr.enabled = false;
         ParticleSystem particlesInstance = Instantiate(smokePrefab, transform.position + smokeSpawnLoc, Quaternion.identity);
-        yield return new WaitForSeconds(3);
+        StartCoroutine(SmokeCollider());
+        yield return new WaitForSeconds(invisTime);
         particlesInstance = Instantiate(smokePrefab, transform.position + smokeSpawnLoc, Quaternion.identity);
+        StartCoroutine(SmokeCollider());
         smr.enabled = true;
         abilityCD = true;
         cdTimer = cdLength;
+    }
+
+    public IEnumerator SmokeCollider()
+    {
+        smokeCollider.enabled = true;
+        yield return new WaitForSeconds(0.25f);
+        smokeCollider.enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        int smokeDamage = 10;
+        PlayerBase otherPlayer = other.gameObject.GetComponent<PlayerBase>();
+
+        if (otherPlayer != null && otherPlayer != baseScript && !other.isTrigger)
+        {
+            StartCoroutine(otherPlayer.TakeDamage(smokeDamage));
+        }
     }
 }
