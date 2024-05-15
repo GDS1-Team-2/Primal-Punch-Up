26cb6fd5bef62da6609e474c7990bcee30a7cdfc
speedyseal69 <111723174+speedyseal69@users.noreply.github.com>
2024-05-15 19:02:24 +1000

    timer countdown animation

--- Primal Punch-Up/Assets/Scripts/UI Scripts/Timer.cs
+++ Primal Punch-Up/Assets/Scripts/UI Scripts/Timer.cs
@@ -9,7 +9,11 @@ public class Timer : MonoBehaviour
     // Start is called before the first frame update
     public float TimeLeft;
     public bool TimerOn = false;
+    private bool countdown = false;
 
+    public GameObject timer1;
+    public GameObject timer2;
+    public GameObject timer3;
     public Text TimerText2;
     public Text TimerText3;
     public Text TimerText4;
@@ -22,6 +26,7 @@ public class Timer : MonoBehaviour
     {
         TimerOn = true;
         RoundsScript = gameObject.GetComponent<RoundsScript>();
+        countdown = false;
     }
 
     void Update()
@@ -49,6 +54,17 @@ public class Timer : MonoBehaviour
     {
         float minutes = Mathf.FloorToInt(currentTime / 60);
         float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        if (countdown == false && seconds < 5.5)
+        {
+            countdown = true;
+            TimerText2.color = Color.red;
+            TimerText3.color = Color.red;
+            TimerText4.color = Color.red;
+            timer1.GetComponent<Animator>().Play("Countdown");
+            timer2.GetComponent<Animator>().Play("Countdown");
+            timer3.GetComponent<Animator>().Play("Countdown");
+        }
 
         TimerText2.text = string.Format("{0:00} : {1:00}", minutes, seconds);
         TimerText3.text = string.Format("{0:00} : {1:00}", minutes, seconds);
